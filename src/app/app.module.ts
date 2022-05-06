f1fb1f27c00aa9bd648922d066f2984061d43e33
Minhaoliu <s1252307@monmouth.edu>
2022-05-06 17:05:13 -0400

    added header and footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ 
+    AppComponent,
+    SignUpComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
